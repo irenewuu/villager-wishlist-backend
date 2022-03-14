0f7f207b182b47ced0ad9ec38dcc1012e5361032
rostrae <irenewu222@gmail.com>
2022-03-14 14:31:18 -0700

    pagination on frontend setup

--- Controller/villagers.js
+++ Controller/villagers.js
@@ -2,18 +2,18 @@ const Villager = require("../Models/villagers")
 
 const getVillagers = (req, res)=>{
     console.log(req.query)
-    Villager.find({name: {$regex:req.query.name}}).skip(Number(req.query.offset)).limit(10).exec((err, data)=>{
-    // Villager.find({}, (err, data)=>{
+    // Villager.find({name: {$regex:req.query.name}}).skip(Number(req.query.offset)).limit(10).exec((err, data)=>{
+    Villager.find({}, (err, data)=>{
         if(err) return res.status(500).send("Something went wrong in db")
-        console.log(data)
+        // console.log(data)
         res.json(data)
     })
 } 
 
 const getVillagerById = (req, res) => {
-    Villager.findById(req.params.id, (err, data) => {
+    Villager.findById(req.params._id, (err, data) => {
         if(err) return res.status(404).send("not found")
-        console.log(res.json(data))
+        console.log(res.data, "data from id")
         res.json(data)
     })
 }
