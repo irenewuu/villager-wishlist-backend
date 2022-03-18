2e97f2afb2510949288a4e5d40cc3d8fb534e60a
mmonay24 <marabelle.monay@gmail.com>
2022-03-18 12:24:23 -0700

    chat socket in progress

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const config = require('./config')
 const cors = require('cors')
 const villagerRouter = require("./Routes/villagerRoute");
 
+
+
 app.use(cors());
 
 app.use(express.json());
@@ -20,3 +22,42 @@ app.use(villagerRouter)
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("server running")
 })
+
+
+
+
+
+// chat function .......................................................
+const http = require('http')
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {cors:{
+    origin:["http://localhost:3001"],
+    allowedHeaders:["Access-Control-Allow-Origin"],
+    credentials:true
+}
+
+});
+
+const PORT = process.env.PORT || 8888;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/server.html');
+  });
+
+  io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    // io.emit("change");
+    socket.on("alert all", ()=>{
+        io.emit("change", socket.id);
+    })
+  });
+
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+  });
+
+//....................................................................
+
+
