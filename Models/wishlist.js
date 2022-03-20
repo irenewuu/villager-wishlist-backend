e5c43a52e81404292ce5d9d565cd4b9d5322a669
rostrae <irenewu222@gmail.com>
2022-03-20 12:03:31 -0700

    villager data simplified

--- Models/wishlist.js
+++ Models/wishlist.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const wishlistSchema = new Schema ({
     user: {type: mongoose.Types.ObjectId, ref: "users"},
-    villager: { type: mongoose.Types.ObjectId, ref: "villagers"}
+    villager: {type: mongoose.Types.ObjectId, ref: "villagers"}
 })
 
 const Wishlist = mongoose.model("wishlists", wishlistSchema)
