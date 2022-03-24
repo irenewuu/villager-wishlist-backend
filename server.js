94cd7140ec28a0f6498340357fe2ceeae583023f
mmonay24 <marabelle.monay@gmail.com>
2022-03-24 12:26:49 -0700

    chat function

--- server.js
+++ server.js
@@ -24,3 +24,38 @@ app.listen(process.env.PORT || 3000, (err)=>{
     if(err) return console.log(err, "port error")
     console.log("server running")
 })
+
+
+
+
+
+// chat function .......................................................
+const http = require('http')
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {cors:{
+    origin:["http://localhost:3001"],
+    allowedHeaders:["Access-Control-Allow-Origin"],
+    credentials:true
+}
+});
+
+const PORT = process.env.PORT || 8888;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/server.html');
+  });
+
+  io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    // io.emit("change");
+    socket.on("alert all", (txt, id)=>{
+        io.emit("change", socket.id, txt, id);
+    })
+  });
+
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+  });
+//....................................................................
