083f2c168004ea62e6d93a8d13f092c0e8baae19
rostrae <irenewu222@gmail.com>
2022-03-24 14:49:14 -0700

    socket.io port change

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const app = express()
 const config = require('./config')
 const cors = require('cors')
+const PORT = process.env.PORT || 3000;
 const villagerRouter = require("./Routes/villagerRoute");
 const userRouter = require("./Routes/userRoute");
 const wishlistRouter = require("./Routes/wishlistRoute");
@@ -20,42 +21,37 @@ app.use(userRouter)
 app.use(wishlistRouter)
 
 
-app.listen(process.env.PORT || 3000, (err)=>{
-    if(err) return console.log(err, "port error")
-    console.log("server running")
-})
-
-
-
-
-
 // chat function .......................................................
 const http = require('http')
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {cors:{
-    origin:["http://localhost:3001"],
-    allowedHeaders:["Access-Control-Allow-Origin"],
-    credentials:true
+  origin:["http://localhost:3001"],
+  allowedHeaders:["Access-Control-Allow-Origin"],
+  credentials:true
 }
 });
 
-const PORT = process.env.PORT || 8888;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/server.html');
-  });
+  res.sendFile(__dirname + '/server.html');
+});
 
-  io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
 
-    // io.emit("change");
-    socket.on("alert all", (txt, id)=>{
-        io.emit("change", socket.id, txt, id);
-    })
-  });
+  // io.emit("change");
+  socket.on("alert all", (txt, id)=>{
+    io.emit("change", socket.id, txt, id);
+  })
+});
 
 server.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
-  });
-//....................................................................
+  console.log(`listening on *:${PORT}`);
+});
+  //....................................................................
+  
+// app.listen(process.env.PORT || 3000, (err)=>{
+//   if(err) return console.log(err, "port error")
+//   console.log("server running")
+// })
