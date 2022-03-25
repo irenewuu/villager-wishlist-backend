ad93831e9e3049ded7f9948df1df798f8067cc0a
rostrae <irenewu222@gmail.com>
2022-03-25 11:11:37 -0700

    socket chat fully connected

--- server.js
+++ server.js
@@ -25,37 +25,26 @@ app.use(villagerRouter)
 app.use(userRouter)
 app.use(wishlistRouter)
 
-var whitelist = ["http://localhost:3001", 'https://acnh-villagerwishlist.vercel.app/']
+// chat function .......................................................
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  allowedHeaders:["Access-Control-Allow-Origin"],
+  origin: ["http://localhost:3000", "http://localhost:3001", 'https://acnh-villagerwishlist.vercel.app/'],
+  allowedHeaders:"Access-Control-Allow-Origin",
   credentials:true
 }
-const io = new Server(server, cors(corsOptions)
-);
+const io = new Server(server, {cors:{corsOptions}});
   
-  
-  // chat function .......................................................
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/server.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-
-  // io.emit("change");
   socket.on("alert all", (txt, id)=>{
     io.emit("change", socket.id, txt, id);
   })
 });
+// chat function end ...................................................
 
 server.listen(PORT, (err)=>{
-    if(err) return console.log(err, "port error")
+  if(err) return console.log(err, "port error")
   console.log(`listening on *:${PORT}`);
 })
